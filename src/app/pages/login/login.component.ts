ecf05ea82762c9c6bff605eea7fe87505fc85187
Seyi Agboola <sagboola@icloud.com>
2024-09-25 07:04:45 -0500

    added input feild and contents to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../../core/common/material/material.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,MaterialModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  show: boolean = false;
+  top: string = "";
+ 
+
+  showCreds(){
+    this.show = !this.show
+     return this.top = this.show ? "0%" : "-54.5%";
+  }
+
+  ngOnInit(): void {
+    this.top = "-54.4%";
+  }
 
 }
